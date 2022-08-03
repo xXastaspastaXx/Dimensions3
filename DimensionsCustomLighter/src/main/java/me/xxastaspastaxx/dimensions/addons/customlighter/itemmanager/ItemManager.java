6ef447c59442f771a97743bd5078bcdcbeeb9465
xXastaspastaXx <39333152+xXastaspastaXx@users.noreply.github.com>
2022-08-03 13:52:57 +0300

    Delete DimensionsCustomLighter directory

--- DimensionsCustomLighter/src/main/java/me/xxastaspastaxx/dimensions/addons/customlighter/itemmanager/ItemManager.java
+++
@@ -1,9 +0,0 @@
-package me.xxastaspastaxx.dimensions.addons.customlighter.itemmanager;
-
-import org.bukkit.inventory.ItemStack;
-
-public abstract class ItemManager {
-
-	public abstract boolean isAccepted(ItemStack lighter);
-
-}
